4e860f07c07a46158a43533ec75774701089ad0c
takehigu26 <t.higuchi@eiya.ulab.cs.waseda.ac.jp>
2021-01-01 17:42:32 +0900

    german changed

--- my_adv_exp/experiments.py
+++ my_adv_exp/experiments.py
@@ -137,6 +137,8 @@ def plot_ranking_histogram2(get_dataset, target_feature, targets=None, epochs=50
     
     print(rankings)
 '''
+
+
 def get_rankings(get_dataset, target_index, targets, epochs=50, lr=0.01, alpha=0.1, batch_size=200, epochs_adv=100, **kwargs):
     rankings_ori = [[] for _ in range(len(targets))]
     rankings_mod = [[] for _ in range(len(targets))]
@@ -189,18 +191,47 @@ def get_ranking(get_dataset, target_index, targets, lr, alpha, batch_size, epoch
         ranking_ori.append(l_ori.index(target[0])+1)
         ranking_mod.append(l_mod.index(target[0])+1)
     return ranking_ori, ranking_mod, my_accuracy_score(yts, model_original(X_test)), my_accuracy_score(yts, model_modified(X_test))
+
+def get_rankings_other(get_dataset, target_index, targets, feature_index, epochs=50, lr=0.01, alpha=0.1, batch_size=200, epochs_adv=100, **kwargs):
+    rankings_ori, rankings_mod = [], []
+    for epoch in range(epochs):
+        ranking_ori, ranking_mod = get_ranking_other(get_dataset, target_index, targets, feature_index,
+                                                                 lr, alpha, batch_size, epochs_adv)
+        rankings_ori.append(ranking_ori)
+        rankings_mod.append(ranking_mod)
+        print("#", end="")
+        if (epoch+1)%10 == 0: print(str(epoch+1), end="")
+    print()   
+    return rankings_ori, rankings_mod    
     
-def plot_histogram(get_dataset, rankings, idx, modified, epochs=50, **kwargs):
-    Xtr, _, _, _ = get_dataset()
-    plt.ylim([0, epochs])
-    figure_title = "rank frequency of feature_" + str(idx)
-    color = 'mediumseagreen'
-    if not modified:
-        figure_title = figure_title + " in original model"
-        color = 'royalblue'
-    else:
-        figure_title = figure_title + " in modified model"
-    plt.title(figure_title, fontsize=15)
-    plt.xlabel('ranking', fontsize=15)
-    plt.ylabel('number of times', fontsize= 15)
-    plt.hist(rankings, bins=Xtr.shape[1], range=(1, Xtr.shape[1]), color=color)
+def get_ranking_other(get_dataset, target_index, targets, feature_index, lr, alpha, batch_size, epochs_adv):
+    ranking_ori, ranking_mod = [], []
+    model_original = get_original_model(get_dataset, batch_size=batch_size, verbose=0)
+    model_modified = get_modified_model(get_dataset, 
+                                        targets, 
+                                        lr=lr,
+                                        alpha=alpha,
+                                        batch_size=batch_size, 
+                                        epochs_adv=epochs_adv,
+                                        model_orig=model_original,
+                                        verbose=0)
+    Xtr, Xts, ytr, yts = get_dataset()
+    X_test, X_train, _, y_train = prep_data(Xtr, Xts, ytr, yts)
+    np.random.seed(1)
+    model_original_np = lambda X: model_original(X).numpy()
+    model_modified_np = lambda X: model_modified(X).numpy()
+    explainer = lime_tabular.LimeTabularExplainer(X_train,
+                                                  feature_names = ["feature_"+str(i) for i in range(Xtr.shape[1])],#df.columns,
+                                                  class_names = [str(i) for i in range(y_train.shape[1])])
+    exp = explainer.explain_instance(X_test[target_index], model_original_np, num_features=Xtr.shape[1], top_labels=1)
+    for key in exp.local_exp.keys():
+        l_ori = list(np.array(exp.local_exp[key], np.int64)[:, 0])
+        break
+    exp = explainer.explain_instance(X_test[target_index], model_modified_np, num_features=Xtr.shape[1], top_labels=1)
+    for key in exp.local_exp.keys():
+        l_mod = list(np.array(exp.local_exp[key], np.int64)[:, 0])
+        break
+    for target in targets:
+        ranking_ori.append(l_ori.index(target[0])+1)
+        ranking_mod.append(l_mod.index(target[0])+1)
+    return l_ori.index(feature_index) + 1, l_mod.index(feature_index) + 1
